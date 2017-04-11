7fa99bcedff616fa4b33ac19627f524bb79caf05
kamalnath <sk.kamalnath@gmail.com>
2017-04-11 08:44:10 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent { 
+
 }
