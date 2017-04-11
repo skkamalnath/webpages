fb6786ce327d1010b24f5e999023ec37eb3aff87
kamalnath <sk.kamalnath@gmail.com>
2017-04-11 08:45:25 +0530

    Create aside.component.ts

---
+++ src/app/aside/aside.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aside-page',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.css']
+})
+
+export class AsideComponent {
+}
