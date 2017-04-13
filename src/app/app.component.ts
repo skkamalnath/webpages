284171ad1a57fa7122113c319be08eaee8320605
kamalnath <sk.kamalnath@gmail.com>
2017-04-13 20:14:39 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent }     from './about/about-us.component';
+import { RegisterComponent }     from './register/register.component';
+import { ContactComponent }     from './contact/contact.component';
 import { AsideComponent } from './aside/aside.component';
 import { LoginComponent } from './login/login.component';
 
@@ -8,6 +11,5 @@ import { LoginComponent } from './login/login.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ImageZoomModule }      from 'angular2-image-zoom';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent }          from './app.component';
+import { HomeComponent }    from './home/home.component';
+import { AboutUsComponent }     from './about/about-us.component';
+import { RegisterComponent }     from './register/register.component';
+import { ContactComponent }     from './contact/contact.component';
 import { AsideComponent } from './aside/aside.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AsideComponent,
-    LoginComponent,
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes),
+    ImageZoomModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    RegisterComponent,
+    ContactComponent,
+    AsideComponent,
+    LoginComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
