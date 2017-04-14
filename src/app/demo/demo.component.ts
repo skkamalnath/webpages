3706115bece0eace67ac7a0ec0021599c7601624
kamalnath <sk.kamalnath@gmail.com>
2017-04-15 00:33:17 +0530

    Add files via upload

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } 	 from '@angular/http';
+
+@Component({
+  selector: 'demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+
+export class DemoComponent implements OnInit {
+
+    public data;
+    public filterQuery = "";
+    public rowsOnPage = 10;
+    public sortBy = "email";
+    public sortOrder = "asc";
+
+    constructor(private http: Http) {
+    }
+
+    ngOnInit(): void {
+        this.http.get('assets/data.json')
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 1000);
+            });
+    }
+
+    public toInt(num: string) {
+        return +num;
+    }
+
+    public sortByWordLength = (a: any) => {
+        return a.city.length;
+    }
+  
+}
