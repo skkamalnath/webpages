011f6aa73a0bb9cdb7b9a0685537dacede13f06d
kamalnath <sk.kamalnath@gmail.com>
2017-04-17 00:18:55 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { ImageZoomModule }      from 'angular2-image-zoom';
-import {DemoModule} from './demo/demo.module';
+import {Table1Module} from './home/table1/table1.module';
+import {Table2Module} from './home/table2/table2.module';
+import {Table3Module} from './home/table3/table3.module';
+import {Table4Module} from './home/table4/table4.module';
 
 import { AppComponent }          from './app.component';
 import { HomeComponent }    from './home/home.component';
-import { DemoComponent }    from './demo/demo.component';
+import { Table1Component }    from './home/table1/table1.component';
+import { Table2Component }    from './home/table2/table2.component';
+import { Table3Component }    from './home/table3/table3.component';
+import { Table4Component }    from './home/table4/table4.component';
 import { AboutUsComponent }     from './about/about-us.component';
 import { RegisterComponent }     from './register/register.component';
 import { ContactComponent }     from './contact/contact.component';
@@ -30,7 +36,10 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes),
     ImageZoomModule,
-    DemoModule
+    Table1Module,
+    Table2Module,
+    Table3Module,
+    Table4Module
   ],
   declarations: [
     AppComponent,
