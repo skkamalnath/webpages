db35a82410c5bdc2619a1998f829409e99ea138f
kamalnath <sk.kamalnath@gmail.com>
2017-04-17 06:21:23 +0530

    Add files via upload

---
+++ src/app/home/table3/table3.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } 	 from '@angular/http';
+
+@Component({
+  selector: 'table3',
+  templateUrl: './table3.component.html',
+  styleUrls: ['./table3.component.css']
+})
+
+
+export class Table3Component implements OnInit {
+
+    public data;
+    public filterQuery = "";
+    public rowsOnPage = 10;
+    public sortBy = "email";
+    public sortOrder = "asc";
+
+    constructor(private http: Http) {
+    }
+
+    ngOnInit(): void {
+        this.http.get('assets/data.json')
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 1000);
+            });
+    }
+
+    public toInt(num: string) {
+        return +num;
+    }
+
+    public sortByWordLength = (a: any) => {
+        return a.city.length;
+    }
+  
+}
---
+++ src/app/home/table3/table3.module.ts
@@ -0,0 +1,21 @@
+import { NgModule }      from '@angular/core';
+import { CommonModule }      from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { DataTableModule } from "angular2-datatable";
+import { HttpModule } from "@angular/http";
+
+import { Table3Component }   from './table3.component';
+import { DataFilterPipe }   from './data-filter.pipe';
+
+@NgModule({
+  imports:      [ 
+    CommonModule, 
+    DataTableModule, 
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [ Table3Component, DataFilterPipe ],
+  exports: [Table3Component]
+})
+
+export class Table3Module { }
