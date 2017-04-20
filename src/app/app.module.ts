66dfb38575255bdf7b7f6956f6c080e5adbc25cd
kamalnath <sk.kamalnath@gmail.com>
2017-04-20 06:21:45 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {Table1Module} from './home/table1/table1.module';
 import {Table2Module} from './home/table2/table2.module';
 import {Table3Module} from './home/table3/table3.module';
 import {Table4Module} from './home/table4/table4.module';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 
 import { AppComponent }          from './app.component';
 import { HomeComponent }    from './home/home.component';
@@ -17,6 +19,7 @@ import { Table4Component }    from './home/table4/table4.component';
 import { AboutUsComponent }     from './about/about-us.component';
 import { RegisterComponent }     from './register/register.component';
 import { ContactComponent }     from './contact/contact.component';
+import { MarkerComponent }     from './contact/marker.component';
 import { AsideComponent } from './aside/aside.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './not-found.component';
@@ -39,7 +42,8 @@ const appRoutes: Routes = [
     Table1Module,
     Table2Module,
     Table3Module,
-    Table4Module
+    Table4Module,
+    AgmCoreModule
   ],
   declarations: [
     AppComponent,
