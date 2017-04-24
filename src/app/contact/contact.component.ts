391b6e1afec876ca541cb6ae457fecbd21d0562b
kamalnath <sk.kamalnath@gmail.com>
2017-04-24 06:12:50 +0530

    Add files via upload

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,4 +5,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 
-export class ContactComponent { }
+export class ContactComponent {
+  // google maps zoom level
+  zoom: number = 8;
+
+  lat: number = 13.077921;
+  lng: number = 80.222802;
+
+    clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
+	mapClicked($event: any) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+
+ markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+  
+ markers: marker[] = [
+	  {
+		  lat: 12.973858,
+		  lng: 80.015982,
+		  label: 'A',
+		  draggable: true
+	  },
+	  {
+		  lat: 13.373858,
+		  lng: 80.215982,
+		  label: 'B',
+		  draggable: false
+	  },
+	  {
+		  lat: 12.723858,
+		  lng: 80.000000,
+		  label: 'C',
+		  draggable: true
+	  }
+  ]
+}
+
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
