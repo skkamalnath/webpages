3c24eda4bb576516a85f4325acc81ef7f1dff547
kamalnath <sk.kamalnath@gmail.com>
2017-04-24 06:14:18 +0530

    Add files via upload

--- src/app/about/about-us.component.ts
+++ src/app/about/about-us.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from './carousel/carousel.component';
+import { NgClassComponent } from './ng/ng-class.component';
+import { NgIfComponent } from './ng/ng-if.component';
+import { NgForComponent } from './ng/ng-for.component';
+import { NgShowComponent } from './ng/ng-show.component';
+import { NgSwitchComponent } from './ng/ng-switch.component';
+import { NgStyleComponent } from './ng/ng-style.component';
 
 @Component({
   templateUrl: 'about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
 })
 
 export class AboutUsComponent { }
