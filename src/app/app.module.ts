507bf1e61b142a59b3af94e1935448c93716dd2e
kamalnath <sk.kamalnath@gmail.com>
2017-04-24 06:12:01 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {Table3Module} from './home/table3/table3.module';
 import {Table4Module} from './home/table4/table4.module';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 
-
 import { AppComponent }          from './app.component';
 import { HomeComponent }    from './home/home.component';
 import { Table1Component }    from './home/table1/table1.component';
@@ -19,10 +18,16 @@ import { Table4Component }    from './home/table4/table4.component';
 import { AboutUsComponent }     from './about/about-us.component';
 import { RegisterComponent }     from './register/register.component';
 import { ContactComponent }     from './contact/contact.component';
-import { MarkerComponent }     from './contact/marker.component';
 import { AsideComponent } from './aside/aside.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './not-found.component';
+import { CarouselComponent } from './about/carousel/carousel.component';
+import { NgClassComponent } from './about/ng/ng-class.component';
+import { NgIfComponent } from './about/ng/ng-if.component';
+import { NgForComponent } from './about/ng/ng-for.component';
+import { NgShowComponent } from './about/ng/ng-show.component';
+import { NgSwitchComponent } from './about/ng/ng-switch.component';
+import { NgStyleComponent } from './about/ng/ng-style.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -43,7 +48,9 @@ const appRoutes: Routes = [
     Table2Module,
     Table3Module,
     Table4Module,
-    AgmCoreModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCnbM7ZeWNqbISqW4RPktimSWNdXZR_21s'
+  })
   ],
   declarations: [
     AppComponent,
@@ -53,7 +60,14 @@ const appRoutes: Routes = [
     ContactComponent,
     AsideComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    CarouselComponent,
+    NgClassComponent,
+    NgIfComponent,
+    NgForComponent,
+    NgShowComponent,
+    NgSwitchComponent,
+    NgStyleComponent
   ],
   bootstrap: [ AppComponent ]
 })
